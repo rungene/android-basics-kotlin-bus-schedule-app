e27b246cd75acaf11f98901653e3b6f21f9945da
rungene <rungenesamuel@gmail.com>
2022-11-24 12:19:00 +0300

    add a method to get the full schedule as well as a filtered schedule by stop name. You can do this by calling the corresponding methods from ScheduleDao.

--- app/src/main/java/com/example/busschedule/database/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/database/viewmodels/BusScheduleViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.busschedule.database.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
 
-class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {}
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
+
+    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+
+}
