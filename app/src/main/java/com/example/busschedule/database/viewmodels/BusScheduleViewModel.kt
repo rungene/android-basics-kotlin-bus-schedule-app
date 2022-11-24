699cad8705de28c9073f44c833879486c0fca1f7
rungene <rungenesamuel@gmail.com>
2022-11-24 12:28:58 +0300

    refactor:create a class, called a factory, that will instantiate view model objects for you.

--- app/src/main/java/com/example/busschedule/database/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/database/viewmodels/BusScheduleViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.busschedule.database.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
 
@@ -9,5 +10,19 @@ class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
     fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
 
     fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
+
+    class BusScheduleViewModelFactory(
+        private val scheduleDao: ScheduleDao
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(BusScheduleViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return BusScheduleViewModel(scheduleDao) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+
+    }
+
 
 }
