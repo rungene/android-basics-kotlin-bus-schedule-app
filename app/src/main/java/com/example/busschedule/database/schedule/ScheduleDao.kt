13c9aaa7f3850cf99541c2b4fb8082728189e293
rungene <rungenesamuel@gmail.com>
2022-11-24 11:46:47 +0300

    Adding a second query, you select all columns from the schedule table. However, you only want results that match the selected stop name

--- app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/ScheduleDao.kt
@@ -8,6 +8,7 @@ interface ScheduleDao {
     @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
     fun getAll(): List<Schedule>
 
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName: String): List<Schedule>
     
-
 }
