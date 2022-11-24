27e3175f0937ec788352ca2517ff87a96d3c46b2
rungene <rungenesamuel@gmail.com>
2022-11-24 13:06:17 +0300

    refactor:The database class allows other classes easy access to the DAO classes. Add an abstract function that returns a ScheduleDao

--- app/src/main/java/com/example/busschedule/database/AppDatabase.kt
+++ app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -1,6 +1,9 @@
 package com.example.busschedule.database
 
 import androidx.room.RoomDatabase
+import com.example.busschedule.database.schedule.ScheduleDao
 
 abstract class AppDatabase: RoomDatabase() {
+    abstract fun scheduleDao(): ScheduleDao
+
 }
