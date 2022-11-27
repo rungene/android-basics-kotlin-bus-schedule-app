39f42bb5fce6821ca477053a55cdb6561c1991bf
rungene <rungenesamuel@gmail.com>
2022-11-27 10:20:47 +0300

    refactor: Adding a BusStopAdapter class

--- app/src/main/java/com/example/busschedule/BusStopListAdapter.kt
+++ app/src/main/java/com/example/busschedule/BusStopListAdapter.kt
@@ -1,2 +1,8 @@
 package com.example.busschedule
 
+import androidx.recyclerview.widget.ListAdapter
+import com.example.busschedule.database.schedule.Schedule
+
+class BusStopAdapter(private val onItemClicked: (Schedule) -> Unit) : ListAdapter<Schedule,
+        BusStopAdapter.BusStopViewHolder>(DiffCallback) {
+}
